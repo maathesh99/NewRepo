083ed13bf6d3afa3b902b4932988e446c67af436
maathesh99 <86334500+maathesh99@users.noreply.github.com>
2024-02-19 23:13:48 +0530

    Update app.py
    updates st.run instead app.py

--- app.py
+++ app.py
@@ -73,5 +73,7 @@ def split_prompt(text, split_length):
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)),debug=True)
 
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=port, debug=True)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port, debug=True)
+    st.run()
